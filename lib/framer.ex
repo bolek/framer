7e72f7f865524b1bded3282105047eb276185f65
Bolek Kurowski <bolek.kurowski@gmail.com>
2020-03-09 22:08:09 -0400

    mix new framer

---
+++ lib/framer.ex
@@ -0,0 +1,18 @@
+defmodule Framer do
+  @moduledoc """
+  Documentation for `Framer`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Framer.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/framer_test.exs
@@ -0,0 +1,8 @@
+defmodule FramerTest do
+  use ExUnit.Case
+  doctest Framer
+
+  test "greets the world" do
+    assert Framer.hello() == :world
+  end
+end
